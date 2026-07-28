d8b7ee5f129e64eb847c02f2be74a39bcfe61c65
rajanvegad106 <rajanvegad106@gmail.com>
2026-07-28 23:56:04 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.rajan.studentsattendance"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.studentattendance.kpqmzx"
+    applicationId = "com.rajan.studentattendance"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
